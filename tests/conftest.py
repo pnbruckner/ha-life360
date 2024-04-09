7e2bfb79153ecee6a1235bd75f432e435ed83751
Phil Bruckner <pnbruckner@gmail.com>
2024-04-09 09:44:04 -0500

    Update testing infrastructure

---
+++ tests/conftest.py
@@ -0,0 +1,12 @@
+"""Life360 test configuration."""
+from __future__ import annotations
+
+import pytest
+
+pytest_plugins = ["pytest_homeassistant_custom_component"]
+
+
+@pytest.fixture(autouse=True)
+def auto_enable_custom_integrations(enable_custom_integrations):
+    """Enable custom integrations."""
+    yield
